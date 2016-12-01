8b2ef5de9acbe21f09fb35d8764a72dda3b8b045
IlianaVachkova <an_vachkova@yahoo.com>
2016-12-01 17:00:25 +0200

    Add data-layer for hotel model.

---
+++ server/data/hotel-data.js
@@ -0,0 +1,46 @@
+/*globals require module Promise*/
+"use strict";
+
+module.exports = function (models) {
+    let Hotel = models.hotel
+
+    return {
+        getHotelById(id) {
+           return new Promise(function(resolve, reject) {
+				Hotel.findOne({ "_id": id }, function(err, result) {
+					if (err) {
+						return reject(err);
+					}
+
+					return resolve(result);
+				});
+			});
+        },
+        getAllHotels() {
+			return new Promise(function(resolve, reject) {
+				Hotel.find(function(err, result) {
+						if (err) {
+							return reject(err);
+						}
+						return resolve(result);
+					});
+			});
+		},
+        createHotel(data) {
+			return new Promise((resolve, reject) => {
+				let hotel = new Hotel ({
+					name: data.name,
+					owner: data.owner,
+				});
+
+				hotel.save((err, createdHotel) => {
+					if (err) {
+						return reject(err);
+					}
+
+					return resolve(createdHotel);
+				});
+			});
+		},
+    }
+}
--- server/data/index.js
+++ server/data/index.js
@@ -4,7 +4,8 @@ const models = require('./models');
 
 const data = {
     users: require("./user-data")({ user: models.user }),
-    pet: require("./pet-data")({ pet: models.pet })//TODO
+    pet: require("./pet-data")({ pet: models.pet }),//TODO
+    hotel: require("./hotel-data")({ hotel: models.hotel})
 };
 
 module.exports = data;
