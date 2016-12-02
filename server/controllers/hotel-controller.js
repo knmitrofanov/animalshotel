dcecb81dba5a938beb7be702f2ab2012f0545594
ilian1902 <ilian.bratanov82@gmail.com>
2016-12-02 21:16:58 +0200

    Fix redirect to home when add hotel.

--- server/controllers/hotel-controller.js
+++ server/controllers/hotel-controller.js
@@ -5,46 +5,45 @@ const UserData = require("../data").users;
 const breedsData = require("../data/models/breeds");
 
 function loadRegisterPage(req, res) {
-	if(!req.isAuthenticated()){
-		return res.redirect("../auth/login");
-	}
-	var user = req.user;
-	res.render("hotel/register", {
-		breeds: breedsData,
-		user: user
-	});
+    if (!req.isAuthenticated()) {
+        return res.redirect("../auth/login");
+    }
+    var user = req.user;
+    res.render("hotel/register", {
+        breeds: breedsData,
+        user: user
+    });
 }
 
 function registerHotel(req, res) {
-	if(!req.isAuthenticated()){
-		return res.redirect("../auth/login");
-	}
-	const body = req.body;
-	var user = req.user;
-		
-	let newHotelData = {
-		name: body.name,
-		owner: user._id,
+    if (!req.isAuthenticated()) {
+        return res.redirect("../auth/login");
+    }
+    const body = req.body;
+    var user = req.user;
+
+    let newHotelData = {
+        name: body.name,
+        owner: user._id,
         address: body.address,
         phoneNumber: body.phoneNumber,
         species: body.species
-	};
+    };
 
-	HotelData
-		.createHotel(newHotelData)
-		.then(() => {
-			//TODO:redirect somewhere
-			res.send(JSON.stringify(newHotelData));
-		})
-		.catch((err) => {
-			res.status(500);
-			res.send(500, "Registration failed\r\n");
-			res.end();
-		});
-	//TODO: Update user (add new hotel to his list hotels)
+    HotelData
+        .createHotel(newHotelData)
+        .then(() => {
+            res.redirect("/");
+        })
+        .catch((err) => {
+            res.status(500);
+            res.send(500, "Registration failed\r\n");
+            res.end();
+        });
+    //TODO: Update user (add new hotel to his list hotels)
 }
 
-module.exports = { 
-	loadRegisterPage, 
-	registerHotel,
+module.exports = {
+    loadRegisterPage,
+    registerHotel,
 };
