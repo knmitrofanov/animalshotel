3d333f6496a9258541403901f1c393bc9901c3cc
ilian1902 <ilian.bratanov82@gmail.com>
2016-12-03 22:58:14 +0200

    Delete service-router.js

--- server/routers/service-router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = {
-    initializeServiceRoutes: function(app, controllers) {
-        app.get("/service/register", controllers.service.loadRegisterPage);
-        app.get("/services", controllers.service.getAllServices);
-        app.post("/service/register", controllers.service.registerService);
-    }
-};
