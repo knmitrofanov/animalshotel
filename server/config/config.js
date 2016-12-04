fd861d16f8020e98bb229841ba14c405b060e5e7
Kalin <kalin.mitrofanov@gmail.com>
2016-12-05 01:34:23 +0200

    fix

--- server/config/config.js
+++ server/config/config.js
@@ -2,13 +2,13 @@ let path = require('path');
 let rootPath = path.normalize(__dirname + '/../../');
 
 const connectionStrings = {
-    production: 'mongodb://admin:123456@ds119548.mlab.com:19548/animalshotel',
-    development: 'mongodb://localhost/animalshotel'
+    production: "mongodb://admin:123456@ds119548.mlab.com:19548/animalshotel",
+    development: "mongodb://localhost/animalshotel"
 };
 
 module.exports = {
     environment: process.env.NODE_ENV || "development",
-    connectionString: connectionStrings[process.env.NODE_ENV || "development"],
+    connectionString: connectionStrings["production"],
     port: process.env.PORT || 3001,
     rootPath
 };
