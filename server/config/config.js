f61056db2c48d9f341e3230ead26c00618774076
ilian1902 <ilian.bratanov82@gmail.com>
2016-12-04 16:22:27 +0200

    Fix connection string for production.

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,15 @@
 let path = require('path');
 let rootPath = path.normalize(__dirname + '/../../');
 
+const connectionStrings = {
+    production: 'mongodb://ilian82:123456@ds113958.mlab.com:13958/proba-db',
+    development: 'mongodb://localhost:27017/animalshotel'
+};
+
 module.exports = {
     development: {
         rootPath: rootPath,
-        db: 'mongodb://localhost:27017/animalshotel',
+        db: connectionStrings[process.env.NODE_ENV || 'development'],
         port: process.env.PORT || 3000
     }
 };
